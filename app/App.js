7f11dc6c562fcce89439af2c50032b92aa4f6fe2
John Kim <kakwak123@gmail.com>
2021-02-06 13:43:15 +1100

    Edited many stuff idk

--- app/App.js
+++ app/App.js
@@ -1,76 +1,13 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { NavigationContainer } from '@react-navigation/native'
+import React from "react"
 
-import Home from "./src/screens/Home";
-import Search from "./src/screens/Search";
-import Clubs from "./src/screens/Clubs";
-import Notifications from "./src/screens/Notifications";
-import Account from "./src/screens/Account";
+// Navigatiors Footer
+import Main from './src/Navigators/Main'
 
-// https://reactnavigation.org/docs/bottom-tab-navigator/
-const Tab = createBottomTabNavigator();
-
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={Search}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="magnify"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Clubs"
-          component={Clubs}
-        />
-        <Tab.Screen
-          name="Notifications"
-          component={Notifications}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="bell"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Account"
-          component={Account}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="account-circle"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+        <Main />
     </NavigationContainer>
-  );
-};
-
-export default App;
+  )
+}
